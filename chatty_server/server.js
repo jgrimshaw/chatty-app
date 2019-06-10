dbec856e5ad4603bdc99575b51d1daeab56dfe25
Jelena Grimshaw <muchstudio@hotmail.com>
2019-06-10 00:52:42 +0000

    fixing issues

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -1,20 +1,14 @@
-// server.js
 
 const express = require('express');
 const WebSocket = require('ws');
-const uuid = require('uuid');
+const uuid = require("uuid");
 const SocketServer = WebSocket.Server;
-
-// Set the port to 3001
 const PORT = 3001;
-
-// Create a new express server
 const server = express()
-   // Make the express server serve static assets (html, javascript, css) from the /public folder
+
   .use(express.static('public'))
   .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
 
-// Create the WebSockets server
 const wss = new SocketServer({ server });
 
 wss.broadcast = (data) => {
@@ -25,29 +19,23 @@ wss.broadcast = (data) => {
     });
   };
 
-function sendNumUsers() {
+function sendNumOfUsers() {
     wss.broadcast({
       type:"numUsers",
       users: usersOnline
-});
+    });
 }
 
-
-let usersOnline = 0; //Variable to keep track of number of users
+let usersOnline = 0;
 wss.on('connection', (ws) => {
-  console.log('Client connected');
-  usersOnline ++; //increases user count everytime someone connects
-  //When someone connects it sends notification to all
-  sendNumUsers();
-  //When someone connects it sends a color to be added to state for that one person
-  ws.send(assignColor());
+  console.log('connected');
+  usersOnline ++;
+  sendNumOfUsers();
 
-//When it recieves a message
   ws.on('message', (message) => {
     messageObj = JSON.parse(message);
     messageObj.id = uuid();
 
-    // check for type
     switch(messageObj.type) {
       case 'incomingNotification' :
         messageObj.type = 'postNotification';
@@ -58,25 +46,16 @@ wss.on('connection', (ws) => {
         messageObj.type = 'postMessage';
         wss.broadcast(messageObj);
         break;
-      case 'incomingPicture' :
-        messageObj.type = 'postPicture';
-        wss.broadcast(messageObj);
-        break;
-      case Default :
-        console.log("For some reason in the default");
+
+      default :
+        console.log("...default");
         break;
     }
   });
 
-wss.on('connection', (ws) => {
-  console.log('Client connected');
-
-  // Set up a callback for when a client closes the socket.
-  //This usually means they closed their browser.
-
   ws.on('close', () => {
-    console.log('Client disconnected')
+    console.log('disconnected');
     usersOnline --;
-    sendNumUsers()
-    });
+    sendNumOfUsers();
+  });
 });
